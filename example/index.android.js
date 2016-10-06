4c2d824947325af3295d8e9a00042b933d543022
Anton Petrov <petrov.aap@gmail.com>
2016-10-06 20:07:21 +0300

    Updated js code base

--- example/index.android.js
+++ example/index.android.js
@@ -1,44 +1,38 @@
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TextInput
-} from 'react-native';
-
+import { AppRegistry, View } from 'react-native';
 import Dropdown from 'tipsi-dropdown';
 
 class example extends Component {
+  handleChanged = (event) => {
+      console.log(event);
+  }
+
   render() {
     return (
       <View>
-      <Dropdown items={["one", "two", "three", "four"]}
-            style={
-                {
-                  position: 'absolute',
-                  top: 25,
-                  left: 25,
-                  width:156,
-                  height:40,
-                  backgroundColor: '#AAAAAA',
-                  borderColor: '#0000FF',
-                  indicatorImageName: 'custom_triangle.png',
-                  fontSize: 18,
-                  separatorHeight: 2,
-                  separatorColor: '#0000FF',
-                  fontName: 'Arial',
-                  textColor: '#000000',
-                  textAlignment: 'Left',
-                  cornerRadius: 20
-                }
-            } />
+        <Dropdown
+          options={['one', 'two', 'three', 'four']}
+          onChange={this.handleChanged}
+          backgroundColor="#AAAAAA"
+          borderColor="#0000FF"
+          indicatorImageName="custom_triangle.png"
+          fontSize={18}
+          separatorHeight={2}
+          separatorColor="#0000FF"
+          fontName="Arial"
+          textColor="#000000"
+          textAlignment="Left"
+          cornerRadius={20}
+          style={{
+            position: 'absolute',
+            top: 25,
+            left: 25,
+            width:156,
+            height:40,
+          }}
+        />
       </View>
     );
-  }
-  _onItemChanged = (event) => {
-      console.log(event)
   }
 }
 
--- index.js
+++ index.js
@@ -1,53 +1,30 @@
-'use strict';
 import React, { Component, PropTypes } from 'react';
-import { requireNativeComponent, View, DeviceEventEmitter } from 'react-native';
+import { requireNativeComponent, View } from 'react-native';
 
-var NativeDropdown = requireNativeComponent('TipsiDropdown', Dropdown);
+const NativeDropdown = requireNativeComponent('TipsiDropdown', Dropdown);
 
-export default class Dropdown extends React.Component {
+export default class Dropdown extends Component {
   static propTypes = {
     ...View.propTypes,
     options: PropTypes.array,
     value: PropTypes.number,
     onChange: PropTypes.func,
-    style: PropTypes.shape({
-      indicatorImageName: PropTypes.style,
-      backgroundColor: PropTypes.string,
-      fontName: PropTypes.string,
-      fontSize: PropTypes.number,
-      textAlignment: PropTypes.string,
-      cornerRadius: PropTypes.number,
-      separatorHeight: PropTypes.number,
-      separatorColor: PropTypes.string,
-      borderWidth: PropTypes.number,
-      borderColor: PropTypes.string,
-      textColor: PropTypes.string
-    })
-  }
-
-  static defaultProps = {
-    onChange: (event) => { console.log(event.nativeEvent); }
+    indicatorImageName: PropTypes.style,
+    backgroundColor: PropTypes.string,
+    fontName: PropTypes.string,
+    fontSize: PropTypes.number,
+    textAlignment: PropTypes.string,
+    cornerRadius: PropTypes.number,
+    separatorHeight: PropTypes.number,
+    separatorColor: PropTypes.string,
+    borderWidth: PropTypes.number,
+    borderColor: PropTypes.string,
+    textColor: PropTypes.string
   }
 
   render() {
     return (
-      <NativeDropdown
-        {...this.props}
-        onChange={this.props.onChange}
-        options={this.props.items}
-        value={this.props.selected}
-        style={this.props.style}
-        indicatorImageName={this.props.style.indicatorImageName}
-        fontName={this.props.style.fontName}
-        fontSize={this.props.style.fontSize}
-        textAlignment={this.props.style.textAlignment}
-        cornerRadius={this.props.style.cornerRadius}
-        separatorHeight={this.props.style.separatorHeight}
-        separatorColor={this.props.style.separatorColor}
-        borderWidth={this.props.style.borderWidth}
-        borderColor={this.props.style.borderColor}
-        textColor={this.props.style.textColor}
-        />
+      <NativeDropdown {...this.props} />
     );
   }
 }
