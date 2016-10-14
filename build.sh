68051d44752f12295dd44074b29c23d2e4d504c8
Anton Petrov <petrov.aap@gmail.com>
2016-10-14 17:34:54 +0300

    Use appium from nade_modules

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ npm i
 # Run appium
 appiumPID=$(ps -A | grep -v grep | grep appium | awk '{print $1}')
 if [ -z $appiumPID ]; then
-  appium > /dev/null 2>&1 &
+  npm run appium > /dev/null 2>&1 &
 else
   echo "appium is already running, restart appium"
   kill -9 $appiumPID
-  appium > /dev/null 2>&1 &
+  npm run appium > /dev/null 2>&1 &
 fi
 # Build android app
 npm run build
