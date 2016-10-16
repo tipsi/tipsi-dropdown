0d8b85fc140c4d5196d0df93b10ad08883ac8c8c
Anton Petrov <petrov.aap@gmail.com>
2016-10-16 16:10:30 +0300

    Changed js component api to be more like Picker from react-native repository

--- example/index.android.js
+++ example/index.android.js
@@ -2,40 +2,56 @@ import React, { Component } from 'react'
 import { AppRegistry, View } from 'react-native'
 import Dropdown from 'tipsi-dropdown'
 
+const dropdownStyle = {
+  position: 'absolute',
+  top: 25,
+  left: 25,
+  width: 156,
+  height: 40,
+}
+
+const dropdownItemStyle = {
+  backgroundColor: '#AAAAAA',
+  borderColor: '#0000FF',
+  fontSize: 18,
+  separatorHeight: 2,
+  separatorColor: '#0000FF',
+  fontName: 'Arial',
+  textColor: '#000000',
+  textAlignment: 'Left',
+  cornerRadius: 20,
+}
+
 class example extends Component {
   state = {
     value: 'three',
-    options: ['one', 'two', 'three', 'four'],
   }
 
-  handleChanged = (event) => {
+  handleChange = (event) => {
     console.log(event.nativeEvent)
+  }
+
+  handleValueChange = (item, index) => {
+    console.log(item, index)
+
+    this.setState({ value: item.value })
   }
 
   render() {
     return (
       <View>
         <Dropdown
-          options={this.state.options}
+          style={dropdownStyle}
+          itemStyle={dropdownItemStyle}
+          value={this.state.value}
+          indicator={require('./plus.png')}
           onChange={this.handleChanged}
-          backgroundColor="#AAAAAA"
-          borderColor="#0000FF"
-          fontSize={18}
-          separatorHeight={2}
-          separatorColor="#0000FF"
-          fontName="Arial"
-          textColor="#000000"
-          textAlignment="Left"
-          cornerRadius={20}
-          indicatorImage={require('./plus.png')}
-          style={{
-            position: 'absolute',
-            top: 25,
-            left: 25,
-            width: 156,
-            height: 40,
-          }}
-        />
+          onValueChange={this.handleValueChange}>
+          <Dropdown.Item label="One" value="one" />
+          <Dropdown.Item label="Two" value="two" />
+          <Dropdown.Item label="Three" value="three" />
+          <Dropdown.Item label="Four" value="four" />
+        </Dropdown>
       </View>
     )
   }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component, Children, PropTypes } from 'react'
 import { requireNativeComponent, View } from 'react-native'
 import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource'
 import ImageSourcePropType from 'react-native/Libraries/Image/ImageSourcePropType'
@@ -6,38 +6,119 @@ import ImageSourcePropType from 'react-native/Libraries/Image/ImageSourcePropTyp
 export default class Dropdown extends Component {
   static propTypes = {
     ...View.propTypes,
-    value: PropTypes.number,
-    options: PropTypes.arrayOf(PropTypes.string),
+    selectedValue: PropTypes.any,
+    indicator: ImageSourcePropType,
+    itemStyle: PropTypes.shape({
+      backgroundColor: PropTypes.string,
+      fontName: PropTypes.string,
+      fontSize: PropTypes.number,
+      textAlignment: PropTypes.string,
+      cornerRadius: PropTypes.number,
+      separatorHeight: PropTypes.number,
+      separatorColor: PropTypes.string,
+      borderWidth: PropTypes.number,
+      borderColor: PropTypes.string,
+      textColor: PropTypes.string,
+    }),
     onChange: PropTypes.func,
-    indicatorImage: ImageSourcePropType,
-    backgroundColor: PropTypes.string,
-    fontName: PropTypes.string,
-    fontSize: PropTypes.number,
-    textAlignment: PropTypes.string,
-    cornerRadius: PropTypes.number,
-    separatorHeight: PropTypes.number,
-    separatorColor: PropTypes.string,
-    borderWidth: PropTypes.number,
-    borderColor: PropTypes.string,
-    textColor: PropTypes.string,
+    onValueChange: PropTypes.func,
+  }
+
+  state = this.stateFromProps(this.props)
+
+  componentWillReceiveProps(nextProps) {
+    this.setState(this.stateFromProps(nextProps))
+  }
+
+  onChange = (event) => {
+    const { onChange, onValueChange } = this.props
+    const { selectedIndex } = this.state
+    const { nativeEvent } = event
+
+    if (onChange) {
+      onChange(event)
+    }
+    if (onValueChange) {
+      onValueChange(
+        nativeEvent.newValue,
+        nativeEvent.newIndex
+      )
+    }
+
+    // The dropdown is a controlled component. This means we expect the
+    // on*Change handlers to be in charge of updating our
+    // `selectedValue` prop. That way they can also
+    // disallow/undo/mutate the selection of certain values. In other
+    // words, the embedder of this component should be the source of
+    // truth, not the native component.
+    if (this.dropdown && selectedIndex !== nativeEvent.newIndex) {
+      this.dropdown.setNativeProps({ selectedIndex })
+    }
+  }
+
+  // Translate Dropdown prop and children into stuff that TipsiDropdown understands.
+  stateFromProps = (props) => {
+    const { children, selectedValue } = props
+    let selectedIndex = 0
+    const items = []
+
+    Children.toArray(children).forEach((child, index) => {
+      const { value, label } = child
+      if (value === selectedValue) {
+        selectedIndex = index
+      }
+      items.push({ value, label })
+    })
+
+    return { items, selectedIndex }
   }
 
   render() {
-    const { indicatorImage, ...props } = this.props
-    const indicatorImageName = indicatorImage &&
-      resolveAssetSource(indicatorImage).uri
+    const { indicator, style, itemStyle } = this.props
+    const { items, selectedIndex } = this.state
+    const indicatorImageName = indicator &&
+      resolveAssetSource(indicator).uri
 
     return (
       <NativeDropdown
+        ref={dropdown => this.dropdown = dropdown}
+        style={style}
+        items={items}
+        selectedIndex={selectedIndex}
         indicatorImageName={indicatorImageName}
-        {...props}
+        onChange={this.onChange}
+        {...itemStyle}
       />
     )
   }
 }
 
+Dropdown.Item = class extends Component {
+  static propTypes = {
+    value: PropTypes.any,
+    label: PropTypes.string,
+  }
+
+  render() {
+    // These items don't get rendered directly.
+    return null
+  }
+}
+
 const NativeDropdown = requireNativeComponent('TipsiDropdown', Dropdown, {
   nativeOnly: {
+    items: true,
+    selectedIndex: true,
     indicatorImageName: true,
+    backgroundColor: true,
+    fontName: true,
+    fontSize: true,
+    textAlignment: true,
+    cornerRadius: true,
+    separatorHeight: true,
+    separatorColor: true,
+    borderWidth: true,
+    borderColor: true,
+    textColor: true,
   },
 })
