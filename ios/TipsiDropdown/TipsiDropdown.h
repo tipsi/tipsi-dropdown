3c3c2ffd059aae9aa55e8397180a5508ee476d51
Anton Petrov <petrov.aap@gmail.com>
2016-10-19 01:20:04 +0300

    Initial build implementation for CocoaPods

---
+++ ios/TipsiDropdown/TipsiDropdown.h
@@ -0,0 +1,13 @@
+//
+//  TipsiDropdown.h
+//  TipsiDropdown
+//
+//  Created by Anton Petrov on 18.10.16.
+//  Copyright © 2016 Tipsi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TipsiDropdown : NSObject
+
+@end
