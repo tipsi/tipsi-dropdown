52411e61a2d6f324d1992b2b5fcabdf6332575dd
Anton Petrov <petrov.aap@gmail.com>
2016-10-19 19:17:08 +0300

    Try to fix android build

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -2,12 +2,6 @@
 
 case "${TRAVIS_OS_NAME}" in
   osx)
-    . $HOME/.nvm/nvm.sh
-    nvm install stable
     gem install cocoapods --pre # Since Travis is not always on latest version
-  ;;
-  linux)
-    . $HOME/.nvm/nvm.sh
-    nvm install stable
   ;;
 esac
