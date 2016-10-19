8ae6680ddb4a984930cfee29a3124fe786d4e6f7
Anton Petrov <petrov.aap@gmail.com>
2016-10-19 13:19:32 +0300

    Some updates for scrips

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -2,10 +2,12 @@
 
 case "${TRAVIS_OS_NAME}" in
   osx)
+    . $HOME/.nvm/nvm.sh
     nvm install stable
-    gem install cocoapods # Since Travis is not always on latest version
+    gem install cocoapods --pre # Since Travis is not always on latest version
   ;;
   linux)
+    . $HOME/.nvm/nvm.sh
     nvm install stable
   ;;
 esac
