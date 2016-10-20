c7db88d7068090481a7a79fc9160e7b48074fa48
Anton Petrov <petrov.aap@gmail.com>
2016-10-20 16:09:15 +0300

    Updated travis build scripts

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,11 +2,9 @@
 
 case "${TRAVIS_OS_NAME}" in
   osx)
-    xcrun simctl list
     cd example
     set -o pipefail && npm run build:ios | xcpretty -c -f `xcpretty-travis-formatter`
-    npm run test:ios || true
-    cat ../appium.out
+    npm run test:ios
   ;;
   linux)
     cd example
