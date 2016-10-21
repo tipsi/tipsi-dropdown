216ad432a602f579c8a32deed4c6ad487853baa9
Anton Petrov <petrov.aap@gmail.com>
2016-10-21 17:53:58 +0300

    Update local ci script to support iOS build and tests

--- scripts/local-ci.sh
+++ scripts/local-ci.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
+isIOS() {
+  [ "$(uname)" == "Darwin" ]
+}
+
+###################
+# BEFORE INSTALL  #
+###################
+
+# Check is OSX
+! isIOS && echo "Current os is not OSX, setup for iOS will be skipped"
 # Go to example project
 cd example
+# Remove tipsi-dropdown dependency
+rm -rf node_modules/tipsi-dropdown
+
+###################
+# INSTALL         #
+###################
+
 # Install dependencies
 npm install
+# Install pods
+isIOS && pod install --project-directory=ios
+
+###################
+# BEFORE BUILD    #
+###################
+
 # Run appium
 appiumPID=$(ps -A | grep -v grep | grep appium | awk '{print $1}')
 if [ -z $appiumPID ]; then
@@ -13,7 +37,21 @@ else
   kill -9 $appiumPID
   npm run appium > /dev/null 2>&1 &
 fi
-# Build android app
-npm run build:android
-# Run e2e tests
-npm test:android
+
+###################
+# BUILD           #
+###################
+
+# Build Android app
+npm run build:android || true
+# Build iOS app
+isIOS && (npm run build:ios || true)
+
+###################
+# TESTS           #
+###################
+
+# Run Android e2e tests
+npm run test:android || true
+# Run iOS e2e tests
+isIOS && (npm run test:ios || true)
