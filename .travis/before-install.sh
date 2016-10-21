14e025fc032b00ddb3af4079127b2d2058696845
Anton Petrov <petrov.aap@gmail.com>
2016-10-21 16:03:33 +0300

    Clean up travis config

--- .travis/before-install.sh
+++ .travis/before-install.sh
@@ -2,6 +2,12 @@
 
 case "${TRAVIS_OS_NAME}" in
   osx)
+    source ~/.nvm/nvm.sh
+    nvm install stable
     gem install cocoapods --pre # Since Travis is not always on latest version
+  ;;
+  linux)
+    source ~/.nvm/nvm.sh
+    nvm install stable
   ;;
 esac
