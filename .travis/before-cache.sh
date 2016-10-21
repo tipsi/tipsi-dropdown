44f4840426e9eb22c661a6a840669998d896dc42
Anton Petrov <petrov.aap@gmail.com>
2016-10-21 15:24:49 +0300

    Move travis related scripts to '.travis' folder

--- .travis/before-cache.sh
+++ .travis/before-cache.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 case "${TRAVIS_OS_NAME}" in
+  osx)
+    rm -f example/node_modules/tipsi-dropdown
+  ;;
   linux)
+    rm -f example/node_modules/tipsi-dropdown
     rm -f $HOME/.gradle/caches/modules-2/modules-2.lock
   ;;
 esac
