99dc504863a9fd941ecb0fa8eb033be8487ac1a2
Anton Petrov <petrov.aap@gmail.com>
2016-10-22 13:44:10 +0300

    Updated travis before cache script

--- .travis/before-cache.sh
+++ .travis/before-cache.sh
@@ -2,10 +2,10 @@
 
 case "${TRAVIS_OS_NAME}" in
   osx)
-    rm -f example/node_modules/tipsi-dropdown
+    rm -rf example/node_modules/tipsi-dropdown
   ;;
   linux)
-    rm -f example/node_modules/tipsi-dropdown
+    rm -rf example/node_modules/tipsi-dropdown
     rm -f $HOME/.gradle/caches/modules-2/modules-2.lock
   ;;
 esac
