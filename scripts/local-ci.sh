2e9626dd4de711282a419d25e6a9d0c5ce8f4565
Anton Petrov <petrov.aap@gmail.com>
2016-12-20 13:35:05 +0700

    Some changes in local-ci script

--- scripts/local-ci.sh
+++ scripts/local-ci.sh
@@ -8,16 +8,25 @@ else
   skip_new=false
 fi
 
+if [[ $@ == *"--use-old"* ]]; then
+  use_old=true
+else
+  use_old=false
+fi
+
 proj_dir_old=example
 proj_dir_new=example_tmp
 
-react_native_ver=$(cd $proj_dir_old && npm view react-native version)
+react_native_version=$(cd $proj_dir_old && npm view react-native version)
+library_name=$(node -p "require('./package.json').name")
 
 files_to_copy=(
-  $proj_dir_old/package.json
-  $proj_dir_old/index.*.js
-  $proj_dir_old/src
-  $proj_dir_old/test
+  package.json
+  index.{ios,android}.js
+  android/app/build.gradle
+  src
+  scripts
+  tests
 )
 
 isOSX() {
@@ -35,19 +44,34 @@ if ! type react-native > /dev/null; then
   npm install -g react-native-cli
 fi
 
-if $skip_new; then
+if ($skip_new && ! $use_old); then
   echo "Creating new example project skipped"
-else
-  # Remove old test project if exist
-  rm -rf $proj_dir_new
-  # Init new test project
-  react-native init $proj_dir_new --version $react_native_ver
+  # Go to new test project
+  cd $proj_dir_new
+elif (! $skip_new && ! $use_old); then
+  echo "Creating new example project"
+  # Remove old test project and tmp dir if exist
+  rm -rf $proj_dir_new tmp
+  # Init new test project in tmp directory
+  mkdir tmp
+  cd tmp
+  react-native init $proj_dir_old --version $react_native_version
+  # Move new project from tmp dir and remove tmp dir
+  cd ..
+  mv tmp/$proj_dir_old $proj_dir_new
+  rm -rf tmp
   # Copy necessary files from example project
   for i in ${files_to_copy[@]}; do
-    if [ -e $i ]; then
-      cp -rp $i $proj_dir_new
+    if [ -e $proj_dir_old/$i ]; then
+      cp -Rp $proj_dir_old/$i $proj_dir_new/$i
     fi
   done
+  # Go to new test project
+  cd $proj_dir_new
+else
+  echo "Using example folder for tests"
+  # Go to old test project
+  cd $proj_dir_old
 fi
 
 # Go to new test project
@@ -60,6 +84,7 @@ cd $proj_dir_new
 # Install dependencies
 npm install
 # Link project
+react-native unlink $library_name
 react-native link
 
 ###################
